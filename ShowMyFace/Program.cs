582a10a9bc1d07dbc8a88e6f900b21a00a3e1866
Sergey-Savosin <savosin_sergey@mail.ru>
2018-02-19 23:31:44 +0300

    Added show my face

---
+++ ShowMyFace/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media.Imaging;
+
+namespace Petzold.ShowMyFace
+{
+    public class ShowMyFace : Window
+    {
+        [STAThread]
+        public static void Main()
+        {
+            Application app = new Application();
+            app.Run(new ShowMyFace());
+        }
+
+        public ShowMyFace()
+        {
+            Title = "Show my face";
+            Uri uri = new Uri("http://www.charlespetzold.com/PetzoldTattoo.jpg");
+            BitmapImage bitmap = new BitmapImage(uri);
+            Image img = new Image();
+            img.Source = bitmap;
+            Content = img;
+        }
+    }
+}
