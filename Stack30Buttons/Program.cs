ebe11b204f6970953129ac2b2bdc143a9d1ab8d3
savosin_sergey@mail.ru <savosin_sergey@mail.ru>
2018-02-23 20:12:23 +0300

    StackPanel, 30 buttons

---
+++ Stack30Buttons/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace Petzold.Stack30Buttons
+{
+    class Stack30Buttons : Window
+    {
+        [STAThread]
+        static void Main()
+        {
+            Application app = new Application();
+            app.Run(new Stack30Buttons());
+        }
+
+        public Stack30Buttons()
+        {
+            Title = "Stack 30 buttons";
+            SizeToContent = SizeToContent.Width;
+            //ResizeMode = ResizeMode.CanMinimize;
+            AddHandler(Button.ClickEvent, new RoutedEventHandler(ButtonOnClick));
+
+            ScrollViewer scroll = new ScrollViewer();
+            scroll.VerticalScrollBarVisibility = ScrollBarVisibility.Auto;
+
+            StackPanel stack = new StackPanel();
+            stack.Orientation = Orientation.Horizontal;
+            stack.Margin = new Thickness(5);
+            scroll.Content = stack;
+            Content = scroll;
+
+            for (int i = 0; i<3; i++)
+            {
+                StackPanel stackChild = new StackPanel();
+                stack.Children.Add(stackChild);
+                for (int j=0; j<20; j++)
+                {
+                    Button btn = new Button();
+                    btn.Content = "Button #" + (10 * i + j + 1);
+                    btn.Margin = new Thickness(5);
+                    stackChild.Children.Add(btn);
+                }
+            }
+        }
+
+        private void ButtonOnClick(object sender, RoutedEventArgs e)
+        {
+            Button btn = e.Source as Button;
+            if (btn != null)
+            {
+                MessageBox.Show("You clicked the button labeled " + (e.Source as Button).Content);
+            }
+            else
+            {
+                MessageBox.Show("Clicked something else: " + e.Source.ToString());
+            }
+        }
+    }
+}
--- StackTenButtons/Program.cs
+++ StackTenButtons/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Media;
 
 namespace Petzold.StackTenButtons
 {
@@ -18,16 +19,25 @@ namespace Petzold.StackTenButtons
             Title = "Stack ten buttons";
 
             StackPanel stack = new StackPanel();
+            stack.Background = Brushes.Aquamarine;
+            //stack.HorizontalAlignment = HorizontalAlignment.Center;
+            stack.Margin = new Thickness(5);
+
+            SizeToContent = SizeToContent.WidthAndHeight;
+            ResizeMode = ResizeMode.CanMinimize;
+            AddHandler(Button.ClickEvent, new RoutedEventHandler(ButtonOnClick));
             Content = stack;
 
             Random rnd = new Random();
             for (int i = 0; i < 10; i++)
             {
                 Button btn = new Button();
+                //btn.HorizontalAlignment = HorizontalAlignment.Center;
+                btn.Margin = new Thickness(5);
                 btn.Name = ((char)('A' + i)).ToString();
                 btn.FontSize += rnd.Next(10);
                 btn.Content = "Button " + btn.Name + " says 'Click me'";
-                btn.Click += ButtonOnClick;
+                //btn.Click += ButtonOnClick;
                 stack.Children.Add(btn);
             }
         }
