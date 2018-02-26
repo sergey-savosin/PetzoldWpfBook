ceadca37da635a219605dfe5248ed5733ab31692
savosin_sergey@mail.ru <savosin_sergey@mail.ru>
2018-02-26 23:26:22 +0300

    DockPanel examples

---
+++ DockAroundTheBlock/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace Petzold.DockAroundTheBlock
+{
+    class DockAroundTheBlock : Window
+    {
+        [STAThread]
+        static void Main()
+        {
+            Application app = new Application();
+            app.Run(new DockAroundTheBlock());
+        }
+
+        public DockAroundTheBlock()
+        {
+            Title = "Dock around the block";
+
+            DockPanel dock = new DockPanel();
+            //dock.LastChildFill = false;
+            Content = dock;
+
+            for (int i = 0; i<17; i++)
+            {
+                Button btn = new Button();
+                btn.Content = "Button # " + (i + 1);
+                btn.HorizontalAlignment = HorizontalAlignment.Center;
+                dock.Children.Add(btn);
+                btn.SetValue(DockPanel.DockProperty, (Dock)(i % 4));
+            }
+        }
+
+    }
+}
---
+++ MeetTheDocker/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
+
+namespace Petzold.MeetTheDocker
+{
+    class MeetTheDocker : Window
+    {
+        [STAThread]
+        static void Main()
+        {
+            Application app = new Application();
+            app.Run(new MeetTheDocker());
+        }
+
+        public MeetTheDocker()
+        {
+            Title = "Meet the docker";
+
+            DockPanel dock = new DockPanel();
+            Content = dock;
+
+            // Create menu
+            Menu menu = new Menu();
+            MenuItem item = new MenuItem();
+            item.Header = "Menu";
+            menu.Items.Add(item);
+
+            DockPanel.SetDock(menu, Dock.Top);
+            dock.Children.Add(menu);
+
+            // Create toolbar
+            ToolBar tool = new ToolBar();
+            tool.Header = "Toolbar";
+
+            DockPanel.SetDock(tool, Dock.Top);
+            dock.Children.Add(tool);
+
+            // Status bar
+            StatusBar status = new StatusBar();
+            StatusBarItem statitem = new StatusBarItem();
+            statitem.Content = "Status";
+            status.Items.Add(statitem);
+
+            DockPanel.SetDock(status, Dock.Bottom);
+            dock.Children.Add(status);
+
+            // list box
+            ListBox lstbox = new ListBox();
+            lstbox.Items.Add("List box item");
+
+            DockPanel.SetDock(lstbox, Dock.Left);
+            dock.Children.Add(lstbox);
+
+            // Text box
+            TextBox txtbox = new TextBox();
+            txtbox.AcceptsReturn = true;
+
+            dock.Children.Add(txtbox);
+
+            txtbox.Focus();
+        }
+    }
+}
