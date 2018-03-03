56696430e87566ff8c17847769b0090c9e77f0fe
savosin_sergey@mail.ru <savosin_sergey@mail.ru>
2018-03-03 08:35:05 +0300

    Play15 example finished

--- Play15/Program.cs
+++ Play15/Program.cs
@@ -3,6 +3,7 @@ using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Controls.Primitives;
 using System.Windows.Input;
+using System.Windows.Threading;
 
 namespace Petzold.Play15
 {
@@ -42,7 +43,7 @@ namespace Petzold.Play15
                 Margin = new Thickness(10),
                 HorizontalAlignment = HorizontalAlignment.Center
             };
-            btn.Click += ScramleOnClick;
+            btn.Click += ScrambleOnClick;
 
             stack.Children.Add(btn);
 
@@ -82,12 +83,78 @@ namespace Petzold.Play15
 
         private void TileOnMouseLeftButtonDown(object sender, MouseButtonEventArgs e)
         {
-            throw new NotImplementedException();
+            Tile tile = sender as Tile;
+
+            int iMove = unigrid.Children.IndexOf(tile);
+            int xMove = iMove % NumberCols;
+            int yMove = iMove / NumberCols;
+
+            if (xMove == xEmpty)
+                while (yMove != yEmpty)
+                    MoveTile(xMove, yEmpty + (yMove - yEmpty) / Math.Abs(yMove - yEmpty));
+            if (yMove == yEmpty)
+                while (xMove != xEmpty)
+                    MoveTile(xEmpty + (xMove - xEmpty) / Math.Abs(xMove - xEmpty), yMove);
         }
 
-        private void ScramleOnClick(object sender, RoutedEventArgs e)
+        protected override void OnKeyDown(KeyEventArgs e)
         {
-            throw new NotImplementedException();
+            base.OnKeyDown(e);
+            switch (e.Key)
+            {
+                case Key.Right: MoveTile(xEmpty - 1, yEmpty); break;
+                case Key.Left: MoveTile(xEmpty + 1, yEmpty); break;
+                case Key.Down: MoveTile(xEmpty, yEmpty - 1); break;
+                case Key.Up: MoveTile(xEmpty, yEmpty + 1); break;
+            }
+        }
+
+        private void MoveTile(int xTile, int yTile)
+        {
+            if ((xTile == xEmpty && yTile == yEmpty) ||
+                xTile < 0 || xTile >= NumberCols ||
+                yTile < 0 || yTile >= NumberRows)
+                return;
+            int iTile = NumberCols * yTile + xTile;
+            int iEmpty = NumberCols * yEmpty + xEmpty;
+
+            UIElement elTile = unigrid.Children[iTile];
+            UIElement elEmpty = unigrid.Children[iEmpty];
+
+            unigrid.Children.RemoveAt(iTile);
+            unigrid.Children.Insert(iTile, elEmptySpare);
+            unigrid.Children.RemoveAt(iEmpty);
+            unigrid.Children.Insert(iEmpty, elTile);
+
+            xEmpty = xTile;
+            yEmpty = yTile;
+            elEmptySpare = elEmpty;
+        }
+
+        private void ScrambleOnClick(object sender, RoutedEventArgs e)
+        {
+            rand = new Random();
+            iCounter = 16 * NumberCols * NumberRows;
+            DispatcherTimer tmr = new DispatcherTimer()
+            {
+                Interval = TimeSpan.FromMilliseconds(10)
+            };
+            tmr.Tick += TimerOnTick;
+            tmr.Start();
+        }
+
+        private void TimerOnTick(object sender, EventArgs e)
+        {
+            for (int i=0; i<5; i++)
+            {
+                MoveTile(xEmpty, yEmpty + rand.Next(3) - 1);
+                MoveTile(xEmpty + rand.Next(3) - 1, yEmpty);
+            }
+
+            if (0 == iCounter --)
+            {
+                (sender as DispatcherTimer).Stop();
+            }
         }
     }
 }
