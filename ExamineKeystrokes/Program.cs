9b53670719bfa3bc6c9ca4f148282facb5ab688d
savosin_sergey <savosin_sergey@mail.ru>
2018-03-09 22:53:06 +0300

    Examine Keystrokes example

---
+++ ExamineKeystrokes/Program.cs
@@ -0,0 +1,124 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: Шелли
+ * Date: 09.03.2018
+ * Time: 22:19
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+using System.Windows.Media;
+
+namespace Petzold.ExamineKeystrokes
+{
+	/// <summary>
+	/// Class with program entry point.
+	/// </summary>
+	public class ExamineKeystrokes : Window
+	{
+		StackPanel stack;
+		ScrollViewer scroll;
+		string strHeader = "Event     Key      Sys-Key   Text " +
+			"Ctrl-Text Sys-Text Ime KeyStates " +
+			"IsDown IsUp IsToggled IsRepeat ";
+		string strFormatKey = "{0,-10}{1,-9}{2,-10}" +
+			"{3,-10}{4,-15}{5,-8}{6,-7}{7,-10}{8,-10}";
+		string strFormatText = "{0,-10} " +
+			"{1,-10}{2,-10}{3,-10}";
+		
+		/// <summary>
+		/// Program entry point.
+		/// </summary>
+		[STAThread]
+		public static void Main()
+		{
+			Application app = new Application();
+			app.Run(new ExamineKeystrokes());
+		}
+		
+		public ExamineKeystrokes()
+		{
+			Title = "Examine Keystrokes";
+			FontFamily = new FontFamily("Courier New");
+			
+			Grid grid = new Grid();
+			Content = grid;
+			
+			// one row = auto, other - for the rest space
+			RowDefinition rowdef = new RowDefinition()
+			{
+				Height = GridLength.Auto
+			};
+			grid.RowDefinitions.Add(rowdef);
+			grid.RowDefinitions.Add(new RowDefinition());
+			
+			// Title output
+			TextBlock textHeader = new TextBlock()
+			{
+				FontWeight = FontWeights.Bold,
+				Text = strHeader
+			};
+			grid.Children.Add(textHeader);
+			
+			// Create StackPanel via child ScrollViewer
+			// for showing events
+			scroll = new ScrollViewer();
+			grid.Children.Add(scroll);
+			Grid.SetRow(scroll, 1);
+			stack = new StackPanel();
+			scroll.Content = stack;
+		}
+		
+		protected override void OnKeyDown(KeyEventArgs args)
+		{
+			base.OnKeyDown(args);
+			DisplayKeyInfo(args);
+		}
+		
+		protected override void OnKeyUp(KeyEventArgs args)
+		{
+			base.OnKeyUp(args);
+			DisplayKeyInfo(args);
+		}
+		
+		protected override void OnTextInput(TextCompositionEventArgs args)
+		{
+			base.OnTextInput(args);
+			string str = String.Format(strFormatText, args.RoutedEvent.Name,
+			                           args.Text,
+			                           args.ControlText,
+			                           args.SystemText);
+			DisplayInfo(str);
+		}
+		
+		void DisplayKeyInfo(KeyEventArgs args)
+		{
+			string str =
+				String.Format(strFormatKey,
+				              args.RoutedEvent.Name,
+				              args.Key,
+				              args.SystemKey,
+				              args.ImeProcessedKey,
+				              args.KeyStates,
+				              args.IsDown,
+				              args.IsUp,
+				              args.IsToggled,
+				              args.IsRepeat);
+			DisplayInfo(str);
+		}
+		
+		void DisplayInfo(string str)
+		{
+			TextBlock text = new TextBlock()
+			{
+				Text = str
+			};
+			stack.Children.Add(text);
+			scroll.ScrollToBottom();
+		}
+		
+	}
+}
---
+++ ShadowTheStylus/Program.cs
@@ -0,0 +1,139 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: Шелли
+ * Date: 09.03.2018
+ * Time: 21:56
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Shapes;
+
+namespace Petzold.ShadowTheStylus
+{
+	/// <summary>
+	/// Class with program entry point.
+	/// </summary>
+	public class ShadowTheStylus : Window
+	{
+		static readonly SolidColorBrush brushStylus = Brushes.Blue;
+		static readonly SolidColorBrush brushShadow = Brushes.LightBlue;
+		static readonly double widthStroke = 96 / 2.54; //1 cm
+		static readonly Vector vectShadow = new Vector(widthStroke / 4, widthStroke / 4);
+		
+		// additional fields for move operations
+		Canvas canv;
+		Polyline polyStylus, polyShadow;
+		bool isDrawing;
+		
+		/// <summary>
+		/// Program entry point.
+		/// </summary>
+		[STAThread]
+		public static void Main()
+		{
+			Application app = new Application();
+			app.Run(new ShadowTheStylus());
+		}
+		
+		public ShadowTheStylus()
+		{
+			Title = "Shadow the stylus";
+			
+			// Create panel Canvas
+			canv = new Canvas();
+			Content = canv;
+		}
+		
+		protected override void OnStylusDown(StylusDownEventArgs args)
+		{
+			base.OnStylusDown(args);
+			Point ptStylus = args.GetPosition(canv);
+			
+			// create base object
+			polyStylus = new Polyline()
+			{
+				Stroke = brushStylus,
+				StrokeThickness = widthStroke,
+				StrokeStartLineCap = PenLineCap.Round,
+				StrokeEndLineCap = PenLineCap.Round,
+				StrokeLineJoin = PenLineJoin.Round,
+				Points = new PointCollection()
+			};
+			polyStylus.Points.Add(ptStylus);
+			
+			// create shadow object
+			polyShadow = new Polyline()
+			{
+				Stroke = brushShadow,
+				StrokeThickness = widthStroke,
+				StrokeStartLineCap = PenLineCap.Round,
+				StrokeEndLineCap = PenLineCap.Round,
+				StrokeLineJoin = PenLineJoin.Round,
+				Points = new PointCollection()
+			};
+			polyShadow.Points.Add(ptStylus + vectShadow);
+			
+			// insert shadow before the front polyline
+			canv.Children.Insert(canv.Children.Count / 2, polyShadow);
+			
+			// front polyline
+			canv.Children.Add(polyStylus);
+			
+			CaptureStylus();
+			isDrawing = true;
+			args.Handled = true;
+		}
+		
+		protected override void OnStylusMove(StylusEventArgs args)
+		{
+			base.OnStylusMove(args);
+			if (isDrawing)
+			{
+				Point ptStylus = args.GetPosition(canv);
+				polyStylus.Points.Add(ptStylus);
+				polyShadow.Points.Add(ptStylus + vectShadow);
+				args.Handled = true;
+			}
+		}
+		
+		protected override void OnStylusUp(StylusEventArgs args)
+		{
+			base.OnStylusUp(args);
+			if (isDrawing)
+			{
+				isDrawing = false;
+				ReleaseStylusCapture();
+				args.Handled = true;
+			}
+		}
+		
+		protected override void OnTextInput(TextCompositionEventArgs args)
+		{
+			base.OnTextInput(args);
+			
+			// Escape finishes drawing
+			if (isDrawing && args.Text.IndexOf('\x1B') != -1)
+			{
+				ReleaseStylusCapture();
+				args.Handled = true;
+			}
+		}
+		
+		protected override void OnLostStylusCapture(StylusEventArgs args)
+		{
+			base.OnLostStylusCapture(args);
+			if (isDrawing)
+			{
+				canv.Children.Remove(polyStylus);
+				canv.Children.Remove(polyShadow);
+				isDrawing = false;
+			}
+		}
+		
+	}
+}
