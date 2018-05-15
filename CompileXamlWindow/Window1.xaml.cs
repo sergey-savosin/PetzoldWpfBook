9e1612f72c348b3341ccd7e433011ffb57cc135d
savosin_sergey@mail.ru <savosin_sergey@mail.ru>
2018-05-15 22:47:18 +0300

    CompileXaml samples

---
+++ CompileXamlWindow/Window1.xaml.cs
@@ -0,0 +1,49 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: Sergey
+ * Date: 15.05.2018
+ * Time: 22:32
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+
+namespace CompileXamlWindow
+{
+	/// <summary>
+	/// Interaction logic for Window1.xaml
+	/// </summary>
+	public partial class Window1 : Window
+	{
+		public Window1()
+		{
+			InitializeComponent();
+			
+			foreach(PropertyInfo prop in typeof(Brushes).GetProperties())
+				lstbox.Items.Add(prop.Name);
+		}
+		
+		void ButtonOnClick(object sender, RoutedEventArgs args)
+		{
+			Button btn = sender as Button;
+			MessageBox.Show("The button labled '" + btn.Content + "' has been clicked", Title);
+		}
+		
+		void ListBoxOnSelection(object sender, SelectionChangedEventArgs args)
+		{
+			//ListBox lstbox = sender as ListBox;
+			string strItem = lstbox.SelectedItem as string;
+			PropertyInfo prop = typeof(Brushes).GetProperty(strItem);
+			elips.Fill = (Brush)prop.GetValue(null, null);
+		}
+	}
+}
---
+++ LoadXamlWindow/Program.cs
@@ -0,0 +1,99 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: Sergey
+ * Date: 15.05.2018
+ * Time: 22:07
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using Microsoft.Win32;
+using System;
+using System.IO;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Markup;
+using System.Xml;
+
+namespace Petzold.LoadXamlFil
+{
+	/// <summary>
+	/// Class with program entry point.
+	/// </summary>
+	public class LoadXamlFile : Window
+	{
+		Frame frame;
+		
+		/// <summary>
+		/// Program entry point.
+		/// </summary>
+		[STAThread]
+		public static void Main()
+		{
+			Application app = new Application();
+			app.Run(new LoadXamlFile());
+		}
+		
+		public LoadXamlFile()
+		{
+			Title = "Load Xaml File";
+			
+			DockPanel dock = new DockPanel();
+			Content = dock;
+			
+			// button
+			Button btn = new Button()
+			{
+				Content = "Open File...",
+				Margin = new Thickness(12),
+				HorizontalAlignment = HorizontalAlignment.Center
+			};
+			btn.Click += ButtonOnClick;
+			dock.Children.Add(btn);
+			DockPanel.SetDock(btn, Dock.Top);
+			
+			// Frame
+			frame = new Frame();
+			dock.Children.Add(frame);
+		}
+
+		void ButtonOnClick(object sender, RoutedEventArgs e)
+		{
+			OpenFileDialog dlg = new OpenFileDialog()
+			{
+				Filter = "XAML files (*.xaml)|*.xaml|All files(*.*)|*.*"
+			};
+			if ((bool)dlg.ShowDialog())
+			{
+				try
+				{
+					// read file
+					using (XmlTextReader xmlreader = new XmlTextReader(dlg.FileName))
+					{
+						object obj = XamlReader.Load(xmlreader);
+						
+						if (obj is Window)
+						{
+							Window win = obj as Window;
+							win.Owner = this;
+							win.Show();
+						}
+						else
+						{
+							frame.Content = obj;
+						}
+						
+					}
+					
+				}
+				catch (Exception exc)
+				{
+					MessageBox.Show(exc.Message, Title);
+				}
+				finally
+				{
+					
+				}
+			}
+		}
+	}
+}
