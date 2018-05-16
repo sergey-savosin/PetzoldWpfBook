2edea64f31676fed8d4e4d7414dc25c45b82103f
savosin_sergey@mail.ru <savosin_sergey@mail.ru>
2018-05-16 22:34:18 +0300

    XAML samples
    Signed-off-by: savosin_sergey@mail.ru <savosin_sergey@mail.ru>

---
+++ UseCustomClass/Window1.xaml.cs
@@ -0,0 +1,38 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: Sergey
+ * Date: 16.05.2018
+ * Time: 21:11
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using Petzold.SelectColorFromGrid;
+
+namespace UseCustomClass
+{
+	/// <summary>
+	/// Interaction logic for Window1.xaml
+	/// </summary>
+	public partial class Window1 : Window
+	{
+		public Window1()
+		{
+			InitializeComponent();
+		}
+		
+		void ColorGridBoxOnSelectionChanged(object sender, SelectionChangedEventArgs args)
+		{
+			ColorGridBox clrbox = args.Source as ColorGridBox;
+			Background = (Brush) clrbox.SelectedValue;
+		}
+	}
+}
