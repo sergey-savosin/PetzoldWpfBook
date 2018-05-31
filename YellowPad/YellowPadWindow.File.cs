b8889fbaafdacb0d6539f56e4cb8e6b085db5397
savosin_sergey@mail.ru <savosin_sergey@mail.ru>
2018-05-31 22:13:40 +0300

    YellowPad sample

---
+++ YellowPad/YellowPadWindow.File.cs
@@ -0,0 +1,111 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: Sergey
+ * Date: 31.05.2018
+ * Time: 21:45
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+using System.IO;
+using System.Windows;
+using System.Windows.Ink;
+using System.Windows.Input;
+using System.Windows.Markup;
+using System.Windows.Media;
+using Microsoft.Win32;
+
+namespace Petzold.YellowPad
+{
+	/// <summary>
+	/// Description of YellowPadWindow_File.
+	/// </summary>
+	public partial class YellowPadWindow : Window
+	{
+		void NewOnExecuted(object sender, ExecutedRoutedEventArgs e)
+		{
+			inkcanv.Strokes.Clear();
+		}
+		
+		void OpenOnExecuted(object sender, ExecutedRoutedEventArgs e)
+		{
+			OpenFileDialog dlg = new OpenFileDialog()
+			{
+				CheckFileExists = true,
+				Filter = "Ink serialized format (*.isf)|*.isf|" +
+					"All files (*.*)|*.*"
+			};
+			
+			if ((bool)dlg.ShowDialog(this))
+			{
+				try
+				{
+					FileStream file = new FileStream(dlg.FileName,
+					                                 FileMode.Open,
+					                                 FileAccess.Read);
+					inkcanv.Strokes = new StrokeCollection(file);
+					file.Close();
+				}
+				catch (Exception exc)
+				{
+					MessageBox.Show(exc.Message, Title);
+				}
+			}
+		}
+
+		void SaveOnExecuted(object sender, ExecutedRoutedEventArgs e)
+		{
+			SaveFileDialog dlg = new SaveFileDialog()
+			{
+				Filter = "Ink serialized format (*.isf)|*.isf|"+
+					"XAML drawing file (*.xaml)|*.xaml|"+
+					"All files (*.*)|*.*"
+			};
+			
+			if ((bool)dlg.ShowDialog(this))
+			{
+				try
+				{
+					FileStream file = new FileStream(
+						dlg.FileName,
+						FileMode.Create,
+						FileAccess.Write);
+					
+					if (dlg.FilterIndex == 1 || dlg.FilterIndex == 3)
+					{
+						inkcanv.Strokes.Save(file);
+					}
+					else
+					{
+						// save strokes in object DrawingGroup
+						DrawingGroup drawgrp = new DrawingGroup();
+						foreach (Stroke strk in inkcanv.Strokes)
+						{
+							Color clr = strk.DrawingAttributes.Color;
+							if (strk.DrawingAttributes.IsHighlighter)
+								clr = Color.FromArgb(128, clr.R, clr.G, clr.B);
+							drawgrp.Children.Add(
+								new GeometryDrawing(
+									new SolidColorBrush(clr),
+									null,
+									strk.GetGeometry()));
+						}
+						XamlWriter.Save(drawgrp, file);
+					}
+					file.Close();
+				}
+				catch (Exception exc)
+				{
+					MessageBox.Show(exc.Message, Title);
+				}
+			}
+		}
+				
+		void CloseOnExecuted(object sender, ExecutedRoutedEventArgs e)
+		{
+			// close the window
+			Close();
+		}
+
+	}
+}
---
+++ YellowPad/YellowPadWindow.xaml.cs
@@ -0,0 +1,122 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: Sergey
+ * Date: 05/29/2018
+ * Time: 22:41
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Shapes;
+
+namespace Petzold.YellowPad
+{
+	/// <summary>
+	/// Interaction logic for Window1.xaml
+	/// </summary>
+	public partial class YellowPadWindow : Window
+	{
+		public static readonly double widthCanvas = 5 * 96;
+		public static readonly double heightCanvas = 7 * 96;
+		
+		public YellowPadWindow()
+		{
+			InitializeComponent();
+			
+			double y = 96;
+			while (y <= heightCanvas)
+			{
+				Line line = new Line()
+				{
+					X1 = 0,
+					Y1 = y,
+					X2 = widthCanvas,
+					Y2 = y,
+					Stroke = Brushes.LightBlue
+				};
+				inkcanv.Children.Add(line);
+				y += 24;
+				
+				// turn off Eraser-Mode menu for nonplanshet
+				if (Tablet.TabletDevices.Count == 0)
+					menuEraserMode.Visibility = Visibility.Collapsed;
+			}
+		}
+		void StylusModeOnOpened(object sender, RoutedEventArgs e)
+		{
+			throw new NotImplementedException();
+		}
+		void EditOnOpened(object sender, RoutedEventArgs e)
+		{
+			throw new NotImplementedException();
+		}
+		void StylusModeOnClick(object sender, RoutedEventArgs e)
+		{
+			throw new NotImplementedException();
+		}
+		void EraserModeOnOpened(object sender, RoutedEventArgs e)
+		{
+			throw new NotImplementedException();
+		}
+		void EraserModeOnClick(object sender, RoutedEventArgs e)
+		{
+			throw new NotImplementedException();
+		}
+		void StylusToolOnClick(object sender, RoutedEventArgs e)
+		{
+			throw new NotImplementedException();
+		}
+		void EraserToolOnClick(object sender, RoutedEventArgs e)
+		{
+			throw new NotImplementedException();
+		}
+		void AboutOnClick(object sender, RoutedEventArgs e)
+		{
+			throw new NotImplementedException();
+		}
+		void FormatOnClick(object sender, RoutedEventArgs e)
+		{
+			throw new NotImplementedException();
+		}
+		void CutCanExecute(object sender, CanExecuteRoutedEventArgs e)
+		{
+			throw new NotImplementedException();
+		}
+		void CutOnExecuted(object sender, ExecutedRoutedEventArgs e)
+		{
+			throw new NotImplementedException();
+		}
+		void CopyOnExecuted(object sender, ExecutedRoutedEventArgs e)
+		{
+			throw new NotImplementedException();
+		}
+		void PasteCanExecute(object sender, CanExecuteRoutedEventArgs e)
+		{
+			throw new NotImplementedException();
+		}
+		void PasteOnExecuted(object sender, ExecutedRoutedEventArgs e)
+		{
+			throw new NotImplementedException();
+		}
+		void DeleteOnExecuted(object sender, ExecutedRoutedEventArgs e)
+		{
+			throw new NotImplementedException();
+		}
+		void SelectAllOnExecuted(object sender, ExecutedRoutedEventArgs e)
+		{
+			throw new NotImplementedException();
+		}
+		void HelpOnExecuted(object sender, ExecutedRoutedEventArgs e)
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
