68763a8a0e2946c74b44efe5330469d8e31b12e8
savosin_sergey@mail.ru <savosin_sergey@mail.ru>
2018-06-07 22:34:06 +0300

    Fixes

---
+++ RoutedEventDemo/Window1.xaml.cs
@@ -0,0 +1,31 @@
+﻿//--------------------------------------------------
+// RoutedEventDemo.cs (c) 2006 by Charles Petzold
+//--------------------------------------------------
+using System;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+using System.Windows.Media;
+
+namespace Petzold.RoutedEventDemo
+{
+    public partial class RoutedEventDemo : Window
+    {
+//        [STAThread]
+//        public static void Main()
+//        {
+//            Application app = new Application();
+//            app.Run(new RoutedEventDemo());
+//        }
+        public RoutedEventDemo()
+        {
+            InitializeComponent();
+        }
+        void MenuItemOnClick(object sender, RoutedEventArgs args)
+        {
+            string str = (args.Source as MenuItem).Header as string;
+            Color clr = (Color)ColorConverter.ConvertFromString(str);
+            txtblk.Foreground = new SolidColorBrush(clr);
+        }
+    }
+}
