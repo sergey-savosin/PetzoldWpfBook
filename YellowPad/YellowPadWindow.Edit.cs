ade25fce2101ec729de3d7716bbd86967e296199
savosin_sergey@mail.ru <savosin_sergey@mail.ru>
2018-06-07 23:02:07 +0300

    git test

--- YellowPad/YellowPadWindow.Edit.cs
+++ YellowPad/YellowPadWindow.Edit.cs
@@ -66,7 +66,7 @@ namespace Petzold.YellowPad
 				Owner = this,
 				Title = "Format selection"
 			};
-			
+
 			// stroke for the first selected stroke
 			StrokeCollection strokes = inkcanv.GetSelectedStrokes();
 			
