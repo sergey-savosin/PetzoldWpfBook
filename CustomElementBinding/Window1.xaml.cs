c910f2054f5753584e1a2908ed718ea26ced4a69
savosin_sergey@mail.ru <savosin_sergey@mail.ru>
2018-06-18 23:02:48 +0300

    Binding samples

---
+++ CustomElementBinding/Window1.xaml.cs
@@ -0,0 +1,31 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: Sergey
+ * Date: 18.06.2018
+ * Time: 22:43
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+
+namespace Petzold.CustomElementBinding
+{
+	/// <summary>
+	/// Interaction logic for Window1.xaml
+	/// </summary>
+	public partial class Window1 : Window
+	{
+		public Window1()
+		{
+			InitializeComponent();
+		}
+	}
+}
