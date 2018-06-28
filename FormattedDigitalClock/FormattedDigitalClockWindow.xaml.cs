d138f305ec9c4e9a0c43ee8747a40b16226ee015
savosin_sergey@mail.ru <savosin_sergey@mail.ru>
2018-06-28 22:34:10 +0300

    Binding samples

---
+++ FormattedDigitalClock/FormattedDigitalClockWindow.xaml.cs
@@ -0,0 +1,31 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: Sergey
+ * Date: 28.06.2018
+ * Time: 22:21
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+
+namespace Petzold.FormattedDigitalClock
+{
+	/// <summary>
+	/// Interaction logic for Window1.xaml
+	/// </summary>
+	public partial class FormattedDigitalClockWindow : Window
+	{
+		public FormattedDigitalClockWindow()
+		{
+			InitializeComponent();
+		}
+	}
+}
